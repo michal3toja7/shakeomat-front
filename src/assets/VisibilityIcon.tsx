8307a12496f950db3bd2cf260ba61ce522293bec
Michał Bruzdowski <michal3toja7@gmail.com>
2022-12-11 19:29:02 +0100

    Added color like argument in Icons

--- src/assets/VisibilityIcon.tsx
+++ src/assets/VisibilityIcon.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-
-const VisibilityIcon: React.FC = () => {
+type Props = {
+    color?: string
+}
+const VisibilityIcon: React.FC<Props> = ({color}) => {
 
     return (
         <svg xmlns="http://www.w3.org/2000/svg"
              className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch" focusable="false"
              aria-hidden="true"
              viewBox="0 0 24 24" data-testid="VisibilityIcon"
-             fill="#ffffff">
+             fill={color || "#ffffff"}>
             <path
                 d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path>
         </svg>
