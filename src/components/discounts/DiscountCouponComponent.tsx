2aac9cc0b20b09d898b69e0b77a134370129ca1d
Michał Bruzdowski <michal3toja7@gmail.com>
2022-12-11 17:50:19 +0100

    Build base of item component

--- src/components/discounts/DiscountCouponComponent.tsx
+++ src/components/discounts/DiscountCouponComponent.tsx
@@ -1,7 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import style from './DiscountCouponComponent.module.css'
 import IDiscountCoupon from "../../types/discountCoupon.type";
-import getDiscountCoupons from "../../services/discount.service";
+import DoneIcon from "../../assets/DoneIcon";
+import LockIcon from "../../assets/LockIcon";
+import VisibilityIcon from "../../assets/VisibilityIcon";
+import GroupIcon from "../../assets/GroupIcon";
 
 
 type DiscountCouponComponentProps = {
@@ -15,19 +18,28 @@ const DiscountCouponComponent: React.FC<DiscountCouponComponentProps> = ({discou
             <img src={discountCoupon.discount_image} alt={discountCoupon.discount_title || ""}></img>
             <div className={style["item-control-container"]}>
                 <div className={`${style["control-item"]} ${style["reserve"]}`}>
-                    <span className={"icon"}></span>
+                    <span className={style["icon"]}>
+                        <LockIcon/>
+                    </span>
                     Rezerwuj
                 </div>
                 <div className={`${style["control-item"]} ${style["show"]}`}>
-                    <span className={"icon"}></span>
-                    Pokaż kod/numer
+                    <span className={style["icon"]}>
+                        <VisibilityIcon/>
+                    </span>
+                    Pokaż
                 </div>
                 <div className={`${style["control-item"]} ${style["make-public"]}`}>
-                    <span className={"icon"}></span>
-                    Przekaż do publicznych
+                    <span className={style["icon"]}>
+                        <GroupIcon/>
+                    </span>
+                    Udostępnij
+                    {/*Do <br/>publicznych*/}
                 </div>
                 <div className={`${style["control-item"]} ${style["use-up"]}`}>
-                    <span className={"icon"}></span>
+                    <span className={style["icon"]}>
+                        <DoneIcon/>
+                    </span>
                     Użyj
                 </div>
 
