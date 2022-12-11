89b0b47f1e2b93ddd2afc9a371baadfb47818f82
Michał Bruzdowski <michal3toja7@gmail.com>
2022-12-11 17:49:34 +0100

    Added assets

---
+++ src/assets/DoneIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const DoneIcon: React.FC = () => {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+             className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch"
+             focusable="false" aria-hidden="true"
+             viewBox="0 0 24 24" data-testid="CheckIcon"
+             fill="#ffffff">
+
+            <path d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path>
+        </svg>
+    )
+}
+
+export default DoneIcon
+
