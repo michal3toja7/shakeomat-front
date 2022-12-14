d79e3c3e605b69ed29f59fcbbef6b1cb4e84c736
Michał Bruzdowski <michal3toja7@gmail.com>
2022-12-14 21:36:15 +0100

    Added animation on show modal

--- src/components/discounts/coupon/CouponModalContainer.tsx
+++ src/components/discounts/coupon/CouponModalContainer.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from './DiscountCouponModal.module.css'
+import getDiscountCoupons from "../../../services/discount.service";
 
 
 type Props = {
@@ -8,10 +9,17 @@ type Props = {
 }
 
 const CouponModalContainer: React.FC<Props> = ({rotated, children}) => {
+    const [initComponent, setInitComponent] = useState<boolean>(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setInitComponent(false)
+        }, 50)
+    }, []);
 
     return (
-        <div className={style["modal-container"]}>
-            <div className={`${style["item-modal"]} ${rotated && style["rotated"]}`}>
+        <div className={`${style["modal-container"]} ${initComponent && style["init"]}`}>
+            <div className={`${style["item-modal"]} ${rotated && style["rotated"]} ${initComponent && style["init"]}`}>
                 {children}
             </div>
         </div>
