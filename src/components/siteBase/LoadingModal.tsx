92164aa7b791ea9f5025e79da642877a5e7e4e38
Michał Bruzdowski <michal3toja7@gmail.com>
2022-12-16 00:22:45 +0100

    Added Loading modal

---
+++ src/components/siteBase/LoadingModal.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./LoadingModal.module.css"
+import LogoSVG from "../../assets/LogoSVG";
+
+type LoadingModalItemProps = {
+    isLoading: boolean
+    loadingMassage?: string
+}
+
+const LoadingModalItem: React.FC<LoadingModalItemProps> = ({isLoading, loadingMassage}: LoadingModalItemProps) => {
+    return (
+        <div className={`${style["loader-container"]}  ${isLoading && style["show-modal"]}`}>
+            <div className={style.loader}>
+                <LogoSVG/>
+            </div>
+            {loadingMassage && (
+                <div className={"load-description"}>
+                    {loadingMassage}
+                </div>
+            )}
+        </div>
+
+    )
+}
+
+
+type LoadingModalProps = {
+    isLoading: boolean
+    loadingMassage?: string
+}
+
+const LoadingModal: React.FC<LoadingModalProps> = ({isLoading, loadingMassage}: LoadingModalProps) => {
+    const portalModalRoot = document.getElementById("modal-root")!
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <LoadingModalItem isLoading={isLoading} loadingMassage={loadingMassage}/>,
+                portalModalRoot
+            )}
+
+        </React.Fragment>
+    )
+}
+
+export default LoadingModal
